083f08ce54bedfe95ebc52d6f8f0bc450fa1b55f
Mongkol7 <thoeungsereymongkol@gmail.com>
2025-10-02 11:10:09 +0700

    Setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import './App.css';
-import ProductContainer from './Components/Product-Container';
-import ProductDetails from './Components/Product-Detail';
-import ProductCard from './Components/Product-Card';
-import HomePage from './Components/HomePage';
-import Checkout from './Components/Checkout';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/ProductContainer" element={<ProductContainer />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/ProductCard" element={<ProductCard />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="*" element={<h1 className="text-3xl font-bold underline">404 Not Found!</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
